e63ca32561034dd166336077c4defeeeb6e9aa73
Daniel J McGoldrick <mcgold@uw.edu>
2019-12-19 16:06:43 -0800

    Update metadataFromHTSfileOfFiles.py
    spaces not a tab issue

--- metadataFromHTSfileOfFiles.py
+++ metadataFromHTSfileOfFiles.py
@@ -176,7 +176,7 @@ class htsFile(pysam.libcalignmentfile.AlignmentFile):
             return target
     def bwa_version(self):
         headerObj=self.header.to_dict()
-	version='.'
+        version='.'
         for pg in headerObj['PG']:
             if ((re.match(re.compile('bwa'),pg['ID']) is not None) | (re.match(re.compile('STAR'),pg['ID']) is not None:)):
                 try:
